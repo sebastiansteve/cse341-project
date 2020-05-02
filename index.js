783eedbc57c2a88aab2b1c0e195769c022deb66e
sebastiansteve <43971058+sebastiansteve@users.noreply.github.com>
2020-05-02 14:28:21 -0600

    Prove week 2

--- index.js
+++ index.js
@@ -19,11 +19,11 @@ const PORT = process.env.PORT || 5000 // So we can run on heroku || (OR) localho
 const app = express();
 
 // Route setup. You can implement more in the future!
-const ta01Routes = require('./routes/ta01');
-const ta02Routes = require('./routes/ta02');
-const ta03Routes = require('./routes/ta03'); 
-const ta04Routes = require('./routes/ta04');
-const prove2Routes = require('./routes/prove2'); 
+const ta01Routes = require('./routes/Team/ta01');
+const ta02Routes = require('./routes/Team/ta02');
+const ta03Routes = require('./routes/Team/ta03'); 
+const ta04Routes = require('./routes/Team/ta04');
+const prove2Routes = require('./routes/Prove/prove2'); 
 
 app.use(express.static(path.join(__dirname, 'public')))
    .set('views', path.join(__dirname, 'views'))
