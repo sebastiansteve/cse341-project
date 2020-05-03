d458f2de16f08c8e1a34933946f7e04cb51d76ef
sebastiansteve <43971058+sebastiansteve@users.noreply.github.com>
2020-05-02 20:08:46 -0600

    Prove week 2 finished

--- routes/Prove/prove2.js
+++ routes/Prove/prove2.js
@@ -1,18 +1,34 @@
 const express = require('express');
 const router = express.Router();
 
+const bookArray = [];
+const summaryArray = [];
+
 router.get('/',(req, res, next) => {
     res.render('pages/prove2', {
         title: 'Book Summary',
         path: '/prove2', // For pug, EJS
+        titles: bookArray,
+        summaries: summaryArray,
         activeTA03: true, // For HBS
         contentCSS: true, // For HBS
     });
 });
 
-router.get('/book/:newTitle/:newSummary', (req, res, next) => {
-    const newTitle = req.params.newTitle;
-    const newSummary = req.params.newSummary;
+router.post('/book', (req, res, next) => {
+    const newTitle = req.body.newTitle;
+    const newSummary = req.body.newSummary;
+
+
+    bookArray.push(newTitle);
+    summaryArray.push(newSummary);
+
+    res.redirect('/prove2/');
+});
+
+/* router.get('/book', (req, res, next) => {
+    const newTitle = req.query.newTitle;
+    const newSummary = req.query.newSummary;
 
     bookArray.push(newTitle);
     summaryArray.push(newSummary);
@@ -25,6 +41,6 @@ router.get('/book/:newTitle/:newSummary', (req, res, next) => {
         activeTA03: true, // For HBS
         contentCSS: true, // For HBS
     });
-});
+}); */
 
 module.exports = router;
